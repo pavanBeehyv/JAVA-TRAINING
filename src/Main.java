20d231cb83659f7cd22d08c155bca0cfed9c0e1d
pavanBeehyv <kotnanapavankumar@gmail.com>
2022-08-23 22:15:28 +0530

    switch

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int m =-9,n =9;
-        int r = m+n;
-        int r1=r-2*m;
-        int r2 = r-2*n;
-        int a = m++; //a==m & then increment value of m
-        int b = ++n; //increment n & assign it's value to b
-        System.out.println(a+""+b);
-        if(r==0){
-            System.out.println("nothing");}
-        else if (r % 2 == 0) {
-            System.out.println("even");
-        }else
-            System.out.println("odd");
-        b= (a%2==0)?0:1; //ternary operator condition?value1:value2
-        System.out.println(b);
-        System.out.println(r);
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(m/n);
-        System.out.println((double)n/m);
-        System.out.println(n%m);
+        int n = 6;
+        switch (n) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+
+
+        }
     }
 }
