6a76b23cddf934f3b811962bef19b55317ff36d9
pavan-kotnana <kotnanapavankumar@gmail.com>
2022-08-23 20:24:01 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int m =7,n =9;
+        int r = m+n;
+        int r1=r-2*m;
+        int r2 = r-2*n;
+        System.out.println(r);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(m/n);
+        System.out.println((double)n/m);
+        System.out.println(n%m);
+    }
+}
