b944ecf1ef338f5265a36a041322a46a55dc0828
pavan-kotnana <kotnanapavankumar@gmail.com>
2022-08-23 20:53:35 +0530

    if else

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int m =7,n =9;
+        int m =-9,n =9;
         int r = m+n;
         int r1=r-2*m;
         int r2 = r-2*n;
+        if(r==0){
+            System.out.println("nothing");}
+        else if (r % 2 == 0) {
+            System.out.println("even");
+        }else
+            System.out.println("odd");
+
         System.out.println(r);
         System.out.println(r1);
         System.out.println(r2);
