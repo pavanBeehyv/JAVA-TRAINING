fea1587cd2a23261ed5fd8965edee139fd51164f
pavanBeehyv <pavankumar.k@beehyv.com>
2022-08-25 16:22:56 +0530

    abstraction example with Number,Integer,Float classes

--- src/AbstractDemo.java
+++ src/AbstractDemo.java
@@ -1,3 +1,7 @@
+//Why abstract class?
+// to have classes with unimplemented generic methods which can be later implemented after the abstract class is extended.
+//object creation or instantiation of abstract class isn't allowed because to avoid bugs
+// since abstract class might have unimplemented methods
 abstract class Human{
 
     abstract public void walk(); //if a method is not defined that need to be abstract
@@ -14,10 +18,21 @@ class Man extends Human{  //when extending abstract class either inheritor class
 
 }
 
+class Printer{
+    public void show(Number i){ //Here Number is ABSTRACT class which is used by Float,Integer,Double,Long,Byte,Short
+        System.out.println(i);
+    }
+}
+
 public class AbstractDemo {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Human obj = new Man();  //We can't create object of abstract class, we can only inherit abstract class
         obj.walk();
+        int i;
+        float j=  10.6f; //primitive datatypes
+        Float k; //class which extends number
+        Printer p = new Printer();
+        p.show(1.9); // here you can pass any number i.e either float,double,Integer because all these classes extends Number class
     }
 
 }
