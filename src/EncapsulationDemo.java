5de0e2c6fc3bc2dd9685b3d9c5b5139ba3a3b59e
pavanBeehyv <pavankumar.k@beehyv.com>
2022-08-25 14:45:17 +0530

    Encapsulation

--- src/EncapsulationDemo.java
+++ src/EncapsulationDemo.java
@@ -4,21 +4,21 @@ class Student{
     private String name;
 
     public int getRollno(){//object does something refer to methods
-        System.out.println("user is accessing the value");  //this way we can maintain the logs
+        System.out.println("user is accessing the rollno");  //this way we can maintain the logs
         return rollno;
     }
     public void setRollno(int rollno){
-        System.out.println("value is changed");
+        System.out.println("rollno is changed");
         this.rollno=rollno;
     }
 
     public String getName() {
-        System.out.println("user is accessing the value");
+        System.out.println("user is accessing the name");
         return name;
     }
 
     public void setName(String name) {
-        System.out.println("value is changed");
+        System.out.println("name is changed");
         this.name = name;
     }
     //getters & setters
@@ -30,5 +30,7 @@ public class EncapsulationDemo {
     s1.setName("pavan");
     s1.setRollno(13);
         System.out.println(s1.getName()+" "+s1.getRollno());
+        System.out.println(s1.getRollno());
+        System.out.println(s1.getName());
     }
 }
