a1b9f45b2e598da9f7a12fa1f183061a3ac9f3cc
pavanBeehyv <pavankumar.k@beehyv.com>
2022-08-25 11:22:30 +0530

    jagged 2D array & enhanced for loop usage

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,11 +1,26 @@
 
 public class ArrayDemo {
     public static void main(String[]args){
-        int[] a={1,2,3,4};
-        for(int i=0;i<4;i++)
-            System.out.println(a[i]);
-;;      for(int k:a){            //enhanced for loop for reading array
-            System.out.print("\n"+k);
-            System.out.println("hi");   //playing around with print & println
+    //jagged array is containing rows with different no. elements
+        int a[][]={
+                {1,2,3,4},
+                {2,4,6,},
+                {2,3,5,7,11}
+        };
+
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.print(a[i][j]+" ");
             }
-}}
+            System.out.println();
+        }
+
+        //enhanced for loop
+        for(int[] k:a) {
+            for(int l:k)
+            System.out.print(l+" ");
+            System.out.println();
+        }
+
+    }
+}
