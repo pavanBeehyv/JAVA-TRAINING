726253ad4756715032ed663eed10293c1cb81c6d
pavanBeehyv <pavankumar.k@beehyv.com>
2022-08-25 12:28:23 +0530

    super() usage to call constructors of parent class

--- src/SuperDemo.java
+++ src/SuperDemo.java
@@ -8,15 +8,17 @@ class A{
 }
 class B extends A{
     public B(){
+        super();
         System.out.println("in B");
     }
     public B(int i){
+        super(i);
         System.out.println("in B int");
     }
 }
 public class SuperDemo {
     public static void main(String args[]){
-    B obj =new B(); //both the constructors of child & parent class are called with obj creation of child class
+    B obj =new B(1); //both the constructors of child & parent class are called with obj creation of child class
                     //this is due to presence of default super() method in child class
     }
 }
