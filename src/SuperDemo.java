eacc9b09dc44a0344968cb296baf78f0627e12be
pavanBeehyv <pavankumar.k@beehyv.com>
2022-08-25 12:26:25 +0530

    parent constructors calling with object creation of child class- super()

---
+++ src/SuperDemo.java
@@ -0,0 +1,22 @@
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+    public A(int i){
+        System.out.println("in A int");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
+    public B(int i){
+        System.out.println("in B int");
+    }
+}
+public class SuperDemo {
+    public static void main(String args[]){
+    B obj =new B(); //both the constructors of child & parent class are called with obj creation of child class
+                    //this is due to presence of default super() method in child class
+    }
+}
